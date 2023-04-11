533fdb77e44523b196011f7d565514e4ca2d7c1e
Caio Cavallari <caio.cavallari@dimensa.com.br>
2023-04-10 22:34:41 -0300

    fix:(Coverage tests)

--- lib/app/modules/shared/data/errors/failures.dart
+++ lib/app/modules/shared/data/errors/failures.dart
@@ -1,28 +1,11 @@
-import 'package:equatable/equatable.dart';
-
-class SeverException extends Equatable implements Exception {
+class SeverException implements Exception {
   final String message;
 
   SeverException(this.message);
-
-  @override
-  List<Object?> get props => [message];
 }
 
-class ParseDataException extends Equatable implements Exception {
+class ParseDataException implements Exception {
   final String message;
 
   const ParseDataException(this.message);
-
-  @override
-  List<Object?> get props => [message];
-}
-
-class UnknowException extends Equatable implements Exception {
-  final String message;
-
-  const UnknowException(this.message);
-
-  @override
-  List<Object?> get props => [message];
 }
