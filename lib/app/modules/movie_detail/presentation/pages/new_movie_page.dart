be825cb037bc5dd69bb197169ea9401f5411909b
Caio Cavallari <caio.cavallari@dimensa.com.br>
2023-04-18 12:33:49 -0300

    fix: adjusts of routes

---
+++ lib/app/modules/movie_detail/presentation/pages/new_movie_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NewMoviePage extends StatefulWidget {
+  const NewMoviePage({super.key});
+
+  @override
+  State<NewMoviePage> createState() => _NewMoviePageState();
+}
+
+class _NewMoviePageState extends State<NewMoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
